8ba81abcd2a395d329533f7d19c2b1354ed16742
JP-Pereira1141 <240001141@esg.ipsantarem.pt>
2025-12-17 10:56:16 +0000

    Atualização do ficheiro OrderUI.sh

--- OrderUI.sh
+++ OrderUI.sh
@@ -1,24 +1,54 @@
 #!/usr/bin/bash
 
-# Menu de escolha de funções.
-echo -e "\e[1;34m================================="
-echo -e "  Bem vindo, faça a sua escolha:      "
-echo -e "=================================\e[0m"
-echo "1) Mover Ficheiro"
-echo "2) Copiar ficheiro"
-echo "3) Ordenar ficheiros"
-echo "4) Sair"
-echo
-read -p "Escolha uma opção: " opcao
+# Funções
+# Menu
+menu() {
+  echo "1) Mover Ficheiros"
+  echo "2) Copiar Ficheiros"
+  echo "3) Ordenar Ficheiros"
+  echo "4) Sair"
+}
 
-# use cases para cada opção.
-case $opcao in
-    1) echo "Opção 1 selecionada";;
-    2) echo "Opção 2 selecionada";;
-    3) echo "Opção 3 selecionada";;
-    4) echo "Opção 4 selecionada, a sair do programa..."
-    exit 0;;
+# Move
+move() {
+    # localizar ficheiro
+    echo "Escreva o caminho de dirétoria onde se encontra o ficheiro que deseja mover. "
+    echo "Escreva o nome do ficheiro que deseja mover: "
+
+    # mover
+}
+
+# Copy
+copy() {
+    # localizar ficheiro
+    echo "Escreva o caminho de dirétoria onde se encontra o ficheiro que deseja copiar. "
+    echo "Escreva o nome do ficheiro que deseja copiar: "
+
+    # colar
+}
+
+# Order
+order() {
+    # escolher dirétoria
+    echo "Escreva o caminho de dirétoria que deseja organisar: "
+
+    # escolher opção
+}
+
+
+# Inicio do programa
+while true; do
+  menu
+  read -p "Opção: " opcao
+
+  case "$opcao" in
+    1) echo "Opção 1 selecionada"; move ;;
+    2) echo "Opção 2 selecionada"; copy;;
+    3) echo "Opção 3 selecionada"; order ;;
+    4) echo "Opção 4 selecionada, a sair do programa..."; exit 0;;
     *) echo "Opção inválida";;
-esac
+  esac
+done
 
-# 
+#Linha de código teste que não deve aparecer nunca
+echo "O programa não encerrou através de um exit."
