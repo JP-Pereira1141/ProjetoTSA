50d05c3707d68a10b98fde2e67353f18189b3e64
JP-Pereira1141 <240001141@esg.ipsantarem.pt>
2025-12-17 10:28:21 +0000

    Modificação de OrderUI.sh

--- OrderUI.sh
+++ OrderUI.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-# Menu de escolha.
+# Menu de escolha de funções.
 echo -e "\e[1;34m================================="
 echo -e "  Bem vindo, faça a sua escolha:      "
 echo -e "=================================\e[0m"
@@ -11,10 +11,14 @@ echo "4) Sair"
 echo
 read -p "Escolha uma opção: " opcao
 
+# use cases para cada opção.
 case $opcao in
     1) echo "Opção 1 selecionada";;
     2) echo "Opção 2 selecionada";;
     3) echo "Opção 3 selecionada";;
-    4) echo "Opção 4 selecionada, a sair do programa...";;
+    4) echo "Opção 4 selecionada, a sair do programa..."
+    exit 0;;
     *) echo "Opção inválida";;
 esac
+
+# 
